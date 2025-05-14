d1faa5643a8aaf0fe6b534c7702fdcb47162b2c0
LaurencePy <la.eaton@gmail.com>
2025-05-14 16:53:51 +0100

    submit button directs to chat. upload cv file limit, file deletion
    Co-authored-by: George <GergT@users.noreply.github.com>
    Co-authored-by: Sawan <SawanUPR@users.noreply.github.com>
    Co-authored-by: sishbish <sishbish@users.noreply.github.com>
    Co-authored-by: Luke Gahng-Guzen <elementary-luke@users.noreply.github.com>
    Co-authored-by: Sherwin  Paul Shelton Edison <SHERZGAMING@users.noreply.github.com>

--- frontend/landingpage.js
+++ frontend/landingpage.js
@@ -1,3 +1,5 @@
+const file = ""
+
 $("#goTag").click(function () {
     $(".mainContainer").addClass("slideUpFadeOut");
     $(".mainContainer").one("animationend", function () {
@@ -5,8 +7,23 @@ $("#goTag").click(function () {
         $(".infoContainer").show();
         $(".infoContainer").addClass("dropsIn")
     });
-})
+});
 
-$("uploadButton").click(function () {
-    
+$(".uploadContainer").click(function () {
+    $('.fileInput').click();
+});
+
+$('#fileInput').change(function () {
+    file = this.files[0];
+    if (file) {
+        alert('Selected file: ' + file.name);
+    }
+});
+
+$(".submitButton").click(function () {
+    $(".infoContainer").addClass("slideUpFadeOut");
+    $(".infoContainer").one("animationend", function () {
+         $(".infoContainer").hide();
+        window.location = "chat.html";
+    });
 });
